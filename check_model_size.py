a7a8fcc858b21645025272a09859974bf0ce69fe
nrupatunga <nrupatunga.tunga@gmail.com>
2021-12-09 18:03:42 +0530

    added shufflenet backbone architecture

--- check_model_size.py
+++ check_model_size.py
@@ -1,8 +1,24 @@
+import cv2
+import numpy as np
 import torch
 
 from models.yolo import Model
 
+# verify the forward function to understand the architecture
+img = cv2.imread('./data/images/zidane.jpg')
+img = cv2.resize(img, (288, 288))
+img = np.transpose(img, (2, 0, 1)).astype(np.float32)
+img = img / 255.
+img_t = torch.from_numpy(img)[None]
+
+model = Model('./models/yolov5-shufflenet.yaml', ch=3, nc=6, anchors=3)
+torch.save(model.state_dict(), 'shufflenet.pth')
+output = model(img_t, profile=True)
+print('-------------------------------------------')
 model = Model('./models/yolov5nC3NB.yaml', ch=3, nc=6, anchors=3)
-torch.save(model.state_dict(), 'model_weights.pth')
-# torch.onnx.export(model=model, args=torch.zeros(1, 3, 288, 288),
-# f='model.onnx', opset_version=11)
+torch.save(model.state_dict(), 'c3nb.pth')
+output = model(img_t, profile=True)
+print('-------------------------------------------')
+model = Model('./models/yolov5n.yaml', ch=3, nc=6, anchors=3)
+torch.save(model.state_dict(), 'c3nb.pth')
+output = model(img_t, profile=True)

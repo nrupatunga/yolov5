c8b8fab06dcfa26f88d430c7c4c36d572ad95f1b
Nrupatunga <nrupatunga.tunga@gmail.com>
2022-03-28 09:31:19 +0000

    few changes to train.sh

--- train.sh
+++ train.sh
@@ -1,9 +1,10 @@
- python train.py \
-	 --img 288 \
-	 --batch 160 \
-	 --epochs 550 \
-	 --data data/simreal-7-withangle/data.yml \
-	 --name fractionfish \
-	 --cfg models/yolov5nff.yaml \
-	 --hyp data/hyps/hyp.scratch.ff.yaml
-	  --weights yolov5n.pt \
+DATA_DIR="/home/kaushikdas/nrupatunga/.exp/datasets/yolov5/fractionfish-sim/data.yaml"
+python train.py \
+	--img 288 \
+	--batch 160 \
+	--epochs 550 \
+	--data $DATA_DIR \
+	--name fractionfish-sim \
+	--cfg models/yolov5nff.yaml \
+	--hyp data/hyps/hyp.scratch.ff.yaml \
+	--weights yolov5n.pt \
